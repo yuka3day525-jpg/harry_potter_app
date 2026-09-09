b69b0d6e6688f416c3c8d5313532201cbeea0cce
yuka3day525-jpg <yuka3day525@gmail.com>
2026-09-09 15:32:43 +0900

    Remove tracked local files

--- .venv/Lib/site-packages/nltk/huggingface/__init__.py
+++
@@ -1,9 +0,0 @@
-# Natural Language Toolkit: HuggingFace dataset integration
-#
-# Copyright (C) 2001-2026 NLTK Project
-# URL: <https://www.nltk.org/>
-# For license information, see LICENSE.TXT
-
-from nltk.huggingface.dataset import download
-
-__all__ = ["download"]
